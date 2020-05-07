0bec1105dddc36ad70a812d1b06d17a77738b7ee
Caerisse <avellanedaandres.08@gmail.com>
2020-05-07 12:15:59 -0300

    Added tabs

--- Covid-19-Analisis.py
+++ Covid-19-Analisis.py
@@ -1,25 +1,23 @@
 import math
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
 import geopandas as gpd
-#import geoplot as gplt
-#import mapclassify as mc
+from shapely.geometry import Polygon, MultiPolygon
 import json
 from bokeh.io import output_notebook, show, output_file
 from bokeh.plotting import figure
 from bokeh.models import GeoJSONDataSource, LinearColorMapper, ColorBar, NumeralTickFormatter
 from bokeh.palettes import brewer
-
 from bokeh.io.doc import curdoc
-from bokeh.models import Slider, HoverTool, Select
-from bokeh.layouts import widgetbox, row, column
+from bokeh.models import Slider, HoverTool, Select, ColumnDataSource, Tabs, Panel
+from bokeh.layouts import row, column, layout
 
-# Extract Data
+###### Extract Data ######
+
 # Data ref: https://www.argentina.gob.ar/coronavirus/informe-diario/abril2020
-
 url = "https://raw.githubusercontent.com/tobiascanavesi/covidarg/master/datoscovid.txt"
 data = pd.read_csv(url, sep = "\t", header = "infer", index_col="Distrito")
+#Correccion de mal formateo de los datos y compatibilidad con el mapa del ign
 data = data.rename(index={'Ciudad de Buenos Aires': 'Ciudad Autónoma de Buenos Aires', 
     'Tierra del Fuego': 'Tierra del Fuego, Antártida e Islas del Atlántico Sur',
     "Catamarca 0 0 0 0": "Catamarca",
@@ -28,47 +26,69 @@ data = data.rename(index={'Ciudad de Buenos Aires': 'Ciudad Autónoma de Buenos 
 data = data.assign(nam=data.index)
 data = data.fillna(0)
 
-casosarg=[[5 ,1],[6,2],[7,9],[8 ,12],[9,17],[10,19],[11 ,21],[12 ,31],[13 ,34],[14 ,45],
+
+# Mapa de argentina con division interprovincial del IGN
+arg = gpd.read_file("provincia.shp", projection="GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137,298.257223563]],PRIMEM['Greenwich',0],UNIT['Degree',0.017453292519943295]]")
+
+# Union de datos coronavirus con mapa
+arg = pd.merge(arg, data, on="nam")
+
+# Quitar todos los poligonos de la antartida y las islas del atlantico sur por motivos visuales 
+# y que son de poca relevancia para el caso de estudio
+points = [(-80.0, -58.0), (-55.0, -58.0), (-55.0,-50.0), (-20.0,-50.0), (-20.0, -90.0), (-80.0, -90.0)]
+antartida_e_islas = gpd.GeoSeries(Polygon(points))
+new_poligon_list = []
+for multipolygon in arg[arg.nam=='Tierra del Fuego, Antártida e Islas del Atlántico Sur'].geometry:
+    for polygon in multipolygon:
+        if not any(gpd.GeoSeries(polygon).intersects(antartida_e_islas)):
+            new_poligon_list.append(polygon)
+
+arg.geometry[16] = MultiPolygon(new_poligon_list)
+
+# Datos cantidad de casos por dia
+casos_arg=[[5 ,1],[6,2],[7,9],[8 ,12],[9,17],[10,19],[11 ,21],[12 ,31],[13 ,34],[14 ,45],
     [15 ,56],[16 ,65],[17 ,79],[18,97],[19,129],[20,158],[21,225],[22,266],[23,301],[24,387],
     [25,502],[26,589],[27,690],[28,745],[29,820],[30,966],[31,1054],[32,1133],[33,1265],
     [34,1353],[35,1451],[36,1554],[37,1628],[38,1795],[39,1894],[40,1975],[41,2142],[42,2208],
     [43,2277],[44,2443],[45,2571],[46,2669],[47,2758],[48,2839],[49,2941],[50,3031],[51,3144],
     [52,3288],[53,3435],[54,3607],[55,3780],[56,3892],[57,4003],[58,4127],[59,4285],[60,4428],[61,4532]]
 
-casosargdf = pd.DataFrame(casosarg, columns=["dias", "casos"])
+casos_arg_df = pd.DataFrame(casos_arg, columns=["dias", "casos"])
 
-#casosargdf.plot(kind="scatter", x="dias", y="casos")
-#plt.show()
 
-arg = gpd.read_file("provincia.shp", projection="GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137,298.257223563]],PRIMEM['Greenwich',0],UNIT['Degree',0.017453292519943295]]")
 
-arg = pd.merge(arg, data, on="nam")
-
-arg_continental = arg[arg.nam != "Tierra del Fuego, Antártida e Islas del Atlántico Sur"]
-#gplt.polyplot(arg_continental)
-#plt.show()
 
 
 #### Bokeh  ####
 
 output_file("index.html")
 
-merged_json = json.loads(arg_continental.to_json())
+
+# Grafico casos por dia
+casos_arg_ds = ColumnDataSource(casos_arg_df)
+casos_dia = figure(plot_width = 900, plot_height = 500, 
+                title = 'Casos de Coronavirus por dia en Argentina',
+                x_axis_label = 'Dias', y_axis_label = 'Casos Confirmados')
+casos_dia.line(source=casos_arg_ds, x='dias', y='casos', line_width=2)#casos_arg_df.dias, casos_arg_df.casos, line_width=2)
+hover_casos_dia = HoverTool(tooltips = [('Dia', '@dias'),
+                                        ('Casos Confirmados', '@casos')])
+casos_dia.add_tools(hover_casos_dia)
+
+# Grafico Mapa
+merged_json = json.loads(arg.to_json())
 json_data = json.dumps(merged_json)
 
-# Input geojson source that contains features for plotting for:
-# initial year 2018 and initial criteria sale_price_median
+# Input geojson source that contains features for plotting:
 geosource = GeoJSONDataSource(geojson = json_data)
 
 
 # Add hover tool
-hover = HoverTool(tooltips = [ ('Provincia','@nam'),
+hover_map = HoverTool(tooltips = [ ('Provincia','@nam'),
                                ('Casos Confirmados', '@Confirmados'),
                                ('Recuperados', '@Recuperados'),
                                ('Fallecidos', '@Fallecidos'),
                                ('Activos', '@Activos')
                                ])
-
 
 # Make a selection object: select
 select = Select(title='Select Criteria:', value='Median Sales Price', options=['Casos Confirmados',
@@ -86,18 +106,12 @@ def update_plot(attr, old, new):
                 }
     input_field = fields[cr]
     
-    # Update the plot based on the changed inputs
-    p = make_plot(input_field)
-    
-    # Update the layout, clear the old document and display the new document
-    layout = column(p, widgetbox(select))
-    curdoc().clear()
-    curdoc().add_root(layout)
-
-
+    map_arg = make_map(input_field)
+    l1.children.pop()
+    l1.children.append(map_arg)
 
 # Create a plotting function
-def make_plot(field_name):    
+def make_map(field_name):    
     # Set the format of the colorbar
     min_range = 0
     max_range = max(arg[field_name])
@@ -106,22 +120,16 @@ def make_plot(field_name):
     if field_name == "Confirmados":
         # Define a sequential multi-hue color palette.
         palette = brewer['Blues'][8]
-        # Reverse color order so that dark blue is highest obesity.
+        # Reverse color order so that dark blue is highest.
         palette = palette[::-1]
     elif field_name == "Activos":
-        # Define a sequential multi-hue color palette.
         palette = brewer['Blues'][8]
-        # Reverse color order so that dark blue is highest obesity.
         palette = palette[::-1]
     elif field_name == "Recuperados":
-        # Define a sequential multi-hue color palette.
         palette = brewer['Greens'][8]
-        # Reverse color order so that dark blue is highest obesity.
         palette = palette[::-1]
     elif field_name == "Fallecidos":
-        # Define a sequential multi-hue color palette.
         palette = brewer['Reds'][8]
-        # Reverse color order so that dark blue is highest obesity.
         palette = palette[::-1]
 
     # Instantiate LinearColorMapper that linearly maps numbers in a range, into a sequence of colors.
@@ -134,78 +142,38 @@ def make_plot(field_name):
 
     # Create figure object.
 
-    p = figure(title = field_name, 
-            plot_height = 900, plot_width = 700,
-            toolbar_location = None)
-    p.xgrid.grid_line_color = None
-    p.ygrid.grid_line_color = None
-    p.axis.visible = False
+    map_arg = figure(title = field_name, 
+            plot_height = 900, plot_width = 700)#,
+            #toolbar_location = None)
+    map_arg.xgrid.grid_line_color = None
+    map_arg.ygrid.grid_line_color = None
+    map_arg.axis.visible = False
 
     # Add patch renderer to figure. 
-    p.patches('xs','ys', source = geosource, fill_color = {'field' : field_name, 'transform' : color_mapper},
+    map_arg.patches('xs','ys', source = geosource, fill_color = {'field' : field_name, 'transform' : color_mapper},
             line_color = 'black', line_width = 0.25, fill_alpha = 1)
   
     # Specify color bar layout.
-    p.add_layout(color_bar, 'right')
+    map_arg.add_layout(color_bar, 'right')
 
     # Add the hover tool to the graph
-    p.add_tools(hover)
-    return p
+    map_arg.add_tools(hover_map)
+    return map_arg
 
+# Attach function to select
 select.on_change('value', update_plot)
 
-input_field = 'Confirmados'
 # Call the plotting function
-p = make_plot(input_field)
+map_arg = make_map('Confirmados')
 
-# Make a column layout of widgetbox(slider) and plot, and add it to the current document
-# Display the current document
-layout = column(p, widgetbox(select))
-curdoc().add_root(layout)
+l1 = layout([   [select],
+                [map_arg]
+            ])
+l2 = layout([[casos_dia]])
 
-show(layout)
+tab1 = Panel(child=l1,title="Mapa Actual")
+tab2 = Panel(child=l2,title="Evolucion")
+tabs = Tabs(tabs=[ tab1, tab2 ])
 
-
-'''
-scheme = mc.Quantiles(arg_continental['Confirmados'], k=7)
-gplt.choropleth(arg_continental, 
-    hue='Confirmados', 
-    cmap='Reds', 
-    edgecolor='white', linewidth=1,
-    legend=True,
-    legend_kwargs={'loc': 'lower right'}, #'orientation': 'horizontal', 
-    scheme=scheme,
-    zorder=1,
-    ax=recuperados
-    )
-#plt.show()
-
-legend_labels=[
-        '<1.4 million', '1.4-3.2 million', '3.2-5.6 million',
-        '5.6-9 million', '9-37 million'
-    ]
-
-
-
-scheme = mc.Quantiles(arg_continental['Confirmados'], k=7)
-confirmados = gplt.cartogram(
-    arg_continental, scale='Confirmados',
-    legend=True, legend_kwargs={'bbox_to_anchor': (1, 0.1)}, legend_var='hue',
-    hue='Confirmados', scheme=scheme, cmap='Reds',
-    limits=(0.5, 1),
-    zorder=1
-)
-
-scheme = mc.NaturalBreaks(arg_continental['Recuperados'], k=7)
-recuperados = gplt.cartogram(
-    arg_continental, scale='Recuperados',
-    legend=True, legend_kwargs={'bbox_to_anchor': (1, 0.9)}, legend_var='hue',
-    hue='Recuperados', scheme=scheme, cmap='Greens',
-    limits=(0.3, 1),
-    zorder=2
-)
-
-    
-gplt.polyplot(arg_continental, facecolor='lightgray', edgecolor='white', ax=recuperados, zorder=0)
-plt.show()
-'''
+curdoc().add_root(tabs)
+show(tabs)
